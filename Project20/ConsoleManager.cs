16446db7e8f02e433a516f9c20e6a34a0b9a2ac4
Minsqui <simonduda12@gmail.com>
2025-04-20 16:01:19 +0200

    JSON Manager
    All JSON related function moved from ConsoleManager to new JSONManager

--- Project20/ConsoleManager.cs
+++ Project20/ConsoleManager.cs
@@ -18,8 +18,8 @@ namespace Project20
     {
         internal Menu activeMenu;
         internal List<Character> characters;
-        internal Dictionary<string, GameClass> classes { get; } = new Dictionary<string, GameClass>();
-        internal Dictionary<string, GameRace> races { get; } = new Dictionary<string, GameRace>();
+        internal Dictionary<string, GameClass> classes { get; private set; }
+        internal Dictionary<string, GameRace> races { get; private set; } 
         private string charactersPath = ".\\data\\characters";
         private string classesPath = ".\\data\\classes";
         private string racesPath = ".\\data\\races";
@@ -36,112 +36,9 @@ namespace Project20
         /// </summary>
         private void OnStart()
         {
-            LoadCharacters();
-            LoadClasses();
-            LoadRaces();
-        }
-
-        /// <summary>
-        /// Loads all character JSONs from charactersPath.
-        /// </summary>
-        private void LoadCharacters()
-        {
-            if (!Directory.Exists(charactersPath))
-            {
-                Directory.CreateDirectory(charactersPath);
-            }
-
-            string[] filePaths = Directory.GetFiles(charactersPath);
-            
-            foreach (var filePath in filePaths)
-            {
-                try
-                {
-                    using (StreamReader r = new StreamReader(filePath))
-                    {
-                        string json = r.ReadToEnd();
-
-                        Character? newCharacter = JsonSerializer.Deserialize<Character>(json);
-
-                        if (newCharacter == null) continue;
-
-                        characters.Add(newCharacter);
-                        newCharacter.filename = Path.GetFileName(filePath);
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Loads all classes JSONs from classesPath.
-        /// </summary>
-        private void LoadClasses()
-        {
-            if (!Directory.Exists(classesPath))
-            {
-                Directory.CreateDirectory(classesPath);
-            }
-
-            string[] filePaths = Directory.GetFiles(classesPath);
-
-            foreach (var filePath in filePaths)
-            {
-                try
-                {
-                    using (StreamReader r = new StreamReader(filePath))
-                    {
-                        string json = r.ReadToEnd();
-
-                        GameClass? newClass = JsonSerializer.Deserialize<GameClass>(json);
-
-                        if (newClass == null) continue;
-
-                        classes.Add(newClass.id, newClass);
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Loads all races JSONs from racesPath.
-        /// </summary>
-        private void LoadRaces()
-        {
-            if (!Directory.Exists(racesPath))
-            {
-                Directory.CreateDirectory(racesPath);
-            }
-
-            string[] filePaths = Directory.GetFiles(racesPath);
-
-            foreach (var filePath in filePaths)
-            {
-                try
-                {
-                    using (StreamReader r = new StreamReader(filePath))
-                    {
-                        string json = r.ReadToEnd();
-
-                        GameRace? newRace = JsonSerializer.Deserialize<GameRace>(json);
-
-                        if (newRace == null) continue;
-
-                        races.Add(newRace.id, newRace);
-                    }
-                }
-                catch
-                {
-                    continue;
-                }
-            }
+            characters = JSONManager.LoadCharacters(charactersPath);
+            classes = JSONManager.LoadClasses(classesPath);
+            races = JSONManager.LoadRaces(racesPath);
         }
 
         /// <summary>
@@ -207,50 +104,16 @@ namespace Project20
         internal void DeleteCharacter(Character character)
         {
             characters.Remove(character);
-
-            if (!Directory.Exists(charactersPath))
-            {
-                return;
-            }
-            string filePath = Path.Combine(charactersPath, character.filename);
-            File.Delete(filePath);
+            JSONManager.DeleteCharacter(character, charactersPath);
         }
 
         /// <summary>
-        /// Saves character to JSON to charactersPath
+        /// Saves character to JSON to charactersPath.
         /// </summary>
         /// <param name="character">Character that is to be saved.</param>
-        /// <exception cref="ArgumentNullException"></exception>
         internal void SaveCharacter(Character character)
         {
-            string fileName;
-
-            if (character == null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            if (!Directory.Exists(charactersPath))
-            {
-                Directory.CreateDirectory(charactersPath);
-            }
-
-            string jsonString = JsonSerializer.Serialize(character);
-
-            //Checking if character has name
-            if (character.Name == Character.nameBaseValue)
-            {
-                //Generating file name
-                fileName = Character.nameBaseValue + $@"{DateTime.Now.Ticks}";
-            }
-            else
-            {
-                fileName = character.Name;
-            }
-
-            character.filename = fileName+".json";
-            string filePath = Path.Combine(charactersPath, character.filename);
-            File.WriteAllText(filePath, jsonString);
+            JSONManager.SaveCharacter(character, charactersPath);
         }
 
         /// <summary>
@@ -285,57 +148,6 @@ namespace Project20
             }
 
             return chosenRace;
-        }
-
-        /// <summary>
-        /// Generates empty GameRace JSON in given path.
-        /// </summary>
-        /// <param name="path">Path were the JSON should be created.</param>
-        public static void GenerateGameRaceEmptyJSON(string path)
-        {
-            string rndName = $"{DateTime.Now.Ticks}";
-            GameRace gameRace = new GameRace() {
-                id = rndName,
-                name = rndName,
-                traits = [new GameRace.Trait("traitName", "traitDescription")]
-            };
-            
-
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-
-            string jsonString = JsonSerializer.Serialize(gameRace);
-
-            string filePath = Path.Combine(path, gameRace.id + ".json");
-            File.WriteAllText(filePath, jsonString);
-        }
-
-        /// <summary>
-        /// Generates empty GameClass JSON in given path.
-        /// </summary>
-        /// <param name="path">Path were the JSON should be created.</param>
-        public static void GenerateGameClassEmptyJSON(string path)
-        {
-            string rndName = $"{DateTime.Now.Ticks}";
-            GameClass gameClass = new GameClass()
-            {
-                id = rndName,
-                name = rndName,
-                features = [new GameClass.Feature(0, "featureDescription")]
-            };
-
-
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-
-            string jsonString = JsonSerializer.Serialize(gameClass);
-
-            string filePath = Path.Combine(path, gameClass.id + ".json");
-            File.WriteAllText(filePath, jsonString);
         }
     }
 }
---
+++ Project20/JSONManager.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Project20
+{
+    internal class JSONManager
+    {
+        /// <summary>
+        /// Deletes character's JSON.
+        /// </summary>
+        /// <param name="character">Character that is to be removed.</param>
+        /// <param name="path">Path to the character folder.</param>
+        public static void DeleteCharacter(Character character, string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+            string filePath = Path.Combine(path, character.filename);
+            File.Delete(filePath);
+        }
+
+        /// <summary>
+        /// Loads all character JSONs from given path.
+        /// <param name="path">Path to the character folder.</param>
+        /// </summary>
+        static public List<Character> LoadCharacters(string path)
+        {
+            List<Character> characters = new();
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string[] filePaths = Directory.GetFiles(path);
+
+            foreach (var filePath in filePaths)
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(filePath))
+                    {
+                        string json = r.ReadToEnd();
+
+                        Character? newCharacter = JsonSerializer.Deserialize<Character>(json);
+
+                        if (newCharacter == null) continue;
+
+                        characters.Add(newCharacter);
+                        newCharacter.filename = Path.GetFileName(filePath);
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            return characters;
+        }
+
+        /// <summary>
+        /// Loads all classes JSONs from given path.
+        /// <param name="path">Path to the class folder.</param>>
+        /// </summary>
+        public static Dictionary<string, GameClass> LoadClasses(string path)
+        {
+            Dictionary<string, GameClass> classes = new();
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string[] filePaths = Directory.GetFiles(path);
+
+            foreach (var filePath in filePaths)
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(filePath))
+                    {
+                        string json = r.ReadToEnd();
+
+                        GameClass? newClass = JsonSerializer.Deserialize<GameClass>(json);
+
+                        if (newClass == null) continue;
+
+                        classes.Add(newClass.id, newClass);
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            return classes;
+        }
+
+        /// <summary>
+        /// Loads all races JSONs from given path.
+        /// <param name="path">Path to the race folder.</param>
+        /// </summary>
+        public static Dictionary<string, GameRace> LoadRaces(string path)
+        {
+            Dictionary<string, GameRace> races = new();
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string[] filePaths = Directory.GetFiles(path);
+
+            foreach (var filePath in filePaths)
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(filePath))
+                    {
+                        string json = r.ReadToEnd();
+
+                        GameRace? newRace = JsonSerializer.Deserialize<GameRace>(json);
+
+                        if (newRace == null) continue;
+
+                        races.Add(newRace.id, newRace);
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            return races;
+        }
+
+        /// <summary>
+        /// Saves character to JSON to given path.
+        /// </summary>
+        /// <param name="character">Character that is to be saved.</param>
+        /// <param name="path">Path to the character folder.</param>>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void SaveCharacter(Character character, string path)
+        {
+            string fileName;
+
+            if (character == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string jsonString = JsonSerializer.Serialize(character);
+
+            //Checking if character has name
+            if (character.Name == Character.nameBaseValue)
+            {
+                //Generating file name
+                fileName = Character.nameBaseValue + $@"{DateTime.Now.Ticks}";
+            }
+            else
+            {
+                fileName = character.Name;
+            }
+
+            character.filename = fileName + ".json";
+            string filePath = Path.Combine(path, character.filename);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        /// <summary>
+        /// Generates empty GameRace JSON in given path.
+        /// </summary>
+        /// <param name="path">Path were the JSON should be created.</param>
+        public static void GenerateGameRaceEmptyJSON(string path)
+        {
+            string rndName = $"{DateTime.Now.Ticks}";
+            GameRace gameRace = new GameRace()
+            {
+                id = rndName,
+                name = rndName,
+                traits = [new GameRace.Trait("traitName", "traitDescription")]
+            };
+
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string jsonString = JsonSerializer.Serialize(gameRace);
+
+            string filePath = Path.Combine(path, gameRace.id + ".json");
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        /// <summary>
+        /// Generates empty GameClass JSON in given path.
+        /// </summary>
+        /// <param name="path">Path were the JSON should be created.</param>
+        public static void GenerateGameClassEmptyJSON(string path)
+        {
+            string rndName = $"{DateTime.Now.Ticks}";
+            GameClass gameClass = new GameClass()
+            {
+                id = rndName,
+                name = rndName,
+                features = [new GameClass.Feature(0, "featureDescription")]
+            };
+
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string jsonString = JsonSerializer.Serialize(gameClass);
+
+            string filePath = Path.Combine(path, gameClass.id + ".json");
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
